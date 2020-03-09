eb354acb61b404082a107b8a8c4869721a4c819b
amanziZidian <amanzi@somaticdestiny.com>
2020-03-09 18:43:08 -0500

    added a video

--- src/codeolis/Amanzi.js
+++ src/codeolis/Amanzi.js
@@ -4,7 +4,18 @@ import '../App.css';
 function Amanzi() {
   return (
     <>
-      <h1>Yahoooooo!!!!! AMANZI!</h1>
+
+   
+   <h1>Section of AMANZI!</h1>
+    <h2> One of my favorite songs:</h2>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/Mj4z2Y1y2Ak" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+
+
+
+
     </>
   );
 }
