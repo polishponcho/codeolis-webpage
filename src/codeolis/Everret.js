834797ffddc873b9db43438ae328d92705a34a02
Espears809 <61995666+Espears809@users.noreply.github.com>
2020-03-09 18:25:45 -0700

    commiting all my changes

--- src/codeolis/Everret.js
+++ src/codeolis/Everret.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../App.css';
-
+import Epicture from './BeanPark.jpg' 
 function Everret() {
   return (
     <>
-      <h1>Everret!</h1>
+      <h1>Everett!</h1>
+      <div> </div>
+      <h1>What's up!!</h1>
+      
+      <img src={Epicture} alt="Posing At the Park"></img>
     </>
   );
 }
