8980f6d3f470c473ec07e934dff30b634335f5e2
amanziZidian <amanzi@somaticdestiny.com>
2020-03-09 19:09:15 -0500

    adding my changes

--- src/codeolis/Amanzi.js
+++ src/codeolis/Amanzi.js
@@ -5,8 +5,12 @@ function Amanzi() {
   return (
     <>
 
+   <h1> AMANZI is a PEACOCK!!!!</h1>
+
+   <iframe src='https://gfycat.com/ifr/DelectableClutteredHeron' frameborder='0' scrolling='no' allowfullscreen width='640' height='308'></iframe><p> <a href="https://gfycat.com/delectableclutteredheron">via Gfycat</a></p>
+
+
    
-   <h1>Section of AMANZI!</h1>
     <h2> One of my favorite songs:</h2>
    <iframe width="560" height="315" src="https://www.youtube.com/embed/Mj4z2Y1y2Ak" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
