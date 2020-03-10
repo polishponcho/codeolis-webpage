9830effe84c03c8ba195163b06edd21b9ed842aa
miguel plancarte <plancarte2@gmail.com>
2020-03-09 20:10:06 -0500

    Miguel added some CSS

--- src/codeolis/Miguel.js
+++ src/codeolis/Miguel.js
@@ -5,6 +5,9 @@ import brittanyHoward from './brittany-howard.jpg'
 function Miguel() {
   return (
     <>
+      <div style={{background:"gray",
+      color: 'white',
+      textAlign: 'center'}}>
       <h1>Miguel is cool and likes music!</h1>
       <p>Miguel is really excited to go to a concert this summer. Miguel bought a ticket to go see one of his favorite musical artists, Brittany Howard. Brittany Howard is a grammy-award winning (4 times) artist best known for her time with her band, The Alabama Shakes. Brittany Howard is touring to St. Louis for her newest album, titled 'Jaime'. 'Jaime' is Brittany Howards debut solo album, dedicated to her late older sister. My favorite song off of the album is called 'Short and Sweet', an acoustic ode to a small-but-mighty love.</p>
       <figure>
@@ -17,6 +20,7 @@ function Miguel() {
       <figcaption>Brittany Howard performing my favorite song, Short and Sweet on The Daily Show. Courtesy of <a href="https://www.youtube.com/watch?v=6la6OHxweak">YouTube</a>.
       </figcaption>
       </figure>
+      </div>
     </>
   );
 }
